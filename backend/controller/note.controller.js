7687cdea6fa9cc0c5fe9701fe89651f83cdfca8b
Rajitkumar <krajit909@gmail.com>
2023-01-23 21:48:39 +0530

    update note created

--- backend/controller/note.controller.js
+++ backend/controller/note.controller.js
@@ -1,5 +1,59 @@
+import NoteSchema from "../models/notesModel.js"
+import asyncHandler from "../utils/asyncHandler.js"
+import CustomError from "../utils/customError.js"
+import sendResponse from "../utils/sendResponse.js"
+
 export const Home = (req, res) => {
     res.status(200).json({
         message: "Hello Notes User"
     })
 }
+
+export const addNote = asyncHandler( async (req, res) => {
+    const { email, name } = req.body;
+
+    if (!(email && name)) {
+        throw new CustomError("All fields are required", 400);
+    }
+
+    const existingUser = await NoteSchema.findOne({ email });
+
+    if (existingUser) {
+        throw new CustomError("User Already exists",400)
+    }
+
+    let data = {
+        email,
+        notes: [{
+            title: `Hello ${name}`,
+            note: "Welcome to Keep-Notes."
+        }]
+    };
+
+    const newNote = await NoteSchema.create(data);
+
+    await newNote.save({
+        validateBeforeSave: false
+    })
+
+    sendResponse(res, newNote);
+
+})
+
+
+export const updateNote =asyncHandler( async (req, res) => {
+
+    const { title, note, email} = req.body;
+
+    if (!(title && note && email)) {
+        throw new CustomError("All fields are required",404)
+    }
+
+    const existingUser = await NoteSchema.findOne({email});
+
+    if (!existingUser) {
+        throw new CustomError("User not found", 400)
+    }
+
+    
+})
