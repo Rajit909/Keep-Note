f8fc7b9b59a2322d7f2e50a3e0b8bfdc0d5c48e7
Rajitkumar <krajit909@gmail.com>
2023-01-23 22:20:17 +0530

    routes added

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,10 +1,22 @@
 import express from "express";
 
-import { Home } from "../controller/note.controller.js";
+import { 
+    Home,
+    addNote,
+    updateNote,
+    getNotes,
+    deleteNote,
+    editNote,
+} from "../controller/note.controller.js";
 
 const Router = express();
 
 Router.get("/", Home)
+Router.post("/addnote", addNote);
+Router.put("/updatenote", updateNote);
+Router.get("/notes/:email", getNotes);
+Router.delete("/deletenote/:email/:id", deleteNote);
+Router.put("/editNote/:id", editNote);
 
 
 export default Router
