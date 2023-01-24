09b586a72b8c2039e2b2d8eea1fd504f454c4bc3
Rajitkumar <krajit909@gmail.com>
2023-01-24 23:26:54 +0530

    component created

---
+++ frontend/src/config/variable.js
@@ -0,0 +1,11 @@
+const config = {
+    API_KEY: process.env.REACT_APP_API_KEY,
+    APP_ID: process.env.REACT_APP_APP_ID,
+    AUTH_DOMAIN: process.env.REACT_APP_AUTH_DOMAIN,
+    STORAGE_BUCKET: process.env.REACT_APP_STORAGE_BUCKET,
+    MESSAGING_SENDER_ID: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    PROJECT_ID: process.env.REACT_APP_PROJECT_ID,
+    MEASUREMENT_ID: process.env.REACT_APP_MEASUREMENT_ID,
+  };
+  export default config;
+  
