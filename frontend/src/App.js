4c768f0e3ba742c8c22d3d4f93dce301a4af563c
Rajitkumar <krajit909@gmail.com>
2023-01-24 23:33:24 +0530

    componet added in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { Routes, Route} from "react-router-dom"
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import Register from './components/Register';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello React app</h1>
+      <Routes>
+        <Route path='/login' element={ <Login/> } />
+        <Route path='/register' element={ <Register/> } />
+        <Route path='/' element={ <Dashboard/> } />
+      </Routes>
+
     </div>
   );
 }
