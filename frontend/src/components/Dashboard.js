d39f91c7818ce78a4a9522fdf26c6d526fe40583
Rajitkumar <krajit909@gmail.com>
2023-01-25 11:41:21 +0530

    bug fixed

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { useEffect, useRef, useState } from 'react'
+import 
+
 
 function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <>
+    <div className='font-bold text-center p-5 bg-orange-400 '>Dashboard</div>
+    
+
+    </>
   )
 }
 
