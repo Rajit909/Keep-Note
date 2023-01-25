bfb6d8b67c47c9e4ae9f0935e41c94ace6dd7559
Rajitkumar <krajit909@gmail.com>
2023-01-25 14:11:08 +0530

    firebase.config initialized

---
+++ frontend/src/Firebase/firebase.config.js
@@ -0,0 +1,28 @@
+import { initializeApp } from "firebase/app";
+import {
+    getAuth,
+    GoogleAuthProvider,
+    FacebookAuthProvider,
+} from "firebase/auth"
+
+import config from "../config/variable";
+
+const firebaseConfig = {
+    apiKey: config.API_KEY,
+    authDomain: config.AUTH_DOMAIN,
+    projectId: config.PROJECT_ID,
+    storageBucket: config.STORAGE_BUCKET,
+    messagingSenderId: config.MESSAGING_SENDER_ID,
+    appId: config.APP_ID,
+    measurementId: config.MEASUREMENT_ID,
+  };
+
+
+//   initialize firebase;
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
+
+const Google_Provider = new GoogleAuthProvider(app)
+const Facebook_Provider = new FacebookAuthProvider(app)
+
+export { auth, Google_Provider, Facebook_Provider};
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,4 +1,14 @@
 import React from 'react'
+import { NavLink, useNavigate } from 'react-router-dom'
+import Social from "./Social.js"
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import auth from "../config/variable"
+
+
+
+
+
+
 
 function Register() {
   return (
