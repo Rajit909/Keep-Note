b37d080a09140bdccb75f8748df6df5da975b6df
Rajitkumar <krajit909@gmail.com>
2023-01-27 23:08:02 +0530

    bug fixed

--- frontend/src/components/Social.js
+++ frontend/src/components/Social.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {auth, G_Provider, F_Provider } from "../Firebase/firebase.config.js"
+import axios from 'axios'
+import {
+  signInWithPopup,
+  GoogleAuthProvider,
+  facebookAuthProvider
+} from "firebase/auth"
+import { useNavigate } from 'react-router-dom'
 
 function Social() {
+
+
   return (
     <div>Social</div>
   )
