03b604a9d9bac9c532a4b21326d84f96241d2ee6
Rajitkumar <krajit909@gmail.com>
2023-01-27 22:09:21 +0530

    navbar created

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -14,7 +14,6 @@ function Login() {
     password: "",
   });
 
-
   const [ errorMessage, setErrorMessage ] = useState("");
   const inputPassRef = useRef();
 
@@ -60,7 +59,94 @@ function Login() {
   };
 
   return (
-    <div>Login</div>
+    <div className="pt-20 md:pt-0">
+      <div className="relative min-h-screen flex flex-col sm:justify-center items-center bg-gray-100 ">
+        <div className="relative sm:max-w-sm w-full">
+          <div className="card bg-blue-400 shadow-lg  w-full h-full rounded-3xl absolute  transform -rotate-6"></div>
+          <div className="card bg-red-400 shadow-lg  w-full h-full rounded-3xl absolute  transform rotate-6"></div>
+          <div className="relative w-full rounded-3xl  px-6 py-4 bg-gray-100 shadow-md">
+            <h1 className="block text-lg mt-3  text-gray-700 text-center font-semibold">
+              Silico Note App
+            </h1>
+            <h1 className="block text-lg mt-3  text-gray-700 text-center font-semibold">
+              Login
+            </h1>
+            <form
+              method="post"
+              onSubmit={submitHandler}
+              encType="multipart/form-data"
+              className="mt-10"
+            >
+              <div>
+                <input
+                  type="email"
+                  placeholder="Email"
+                  className="mt-1 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg pl-4"
+                  name="email"
+                  value={userDetails.email}
+                  onChange={(e) =>
+                    setUserDetails({ ...userDetails, email: e.target.value })
+                  }
+                />
+              </div>
+
+              <div className="mt-7">
+                <input
+                  ref={inputPassRef}
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  className="mt-1 block w-full border-none bg-gray-100 h-11 rounded-xl shadow-lg pl-4"
+                  value={userDetails.password}
+                  onChange={(e) =>
+                    setUserDetails({ ...userDetails, password: e.target.value })
+                  }
+                />
+              </div>
+              <div className="mt-2">
+                <input
+                  type="checkbox"
+                  name="checkbox"
+                  className="mr-2 ml-2"
+                  onClick={showPassword}
+                />
+                <span>Show password</span>
+              </div>
+
+              <div className="mt-7">
+                <button className="bg-blue-500 w-full py-3 rounded-xl text-white shadow-xl hover:shadow-inner focus:outline-none transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105">
+                  Login
+                </button>
+              </div>
+              <h4 className="text-sm font-semibold mt-2 text-red-600 text-center">
+                {erorMessage}
+              </h4>
+              <div className="flex mt-7 items-center text-center">
+                <hr className="border-gray-300 border-1 w-full rounded-md" />
+                <label className="block font-medium text-sm text-gray-600 w-full">
+                  Login Option
+                </label>
+                <hr className="border-gray-300 border-1 w-full rounded-md" />
+              </div>
+
+              <SocialComponent />
+
+              <div className="mt-7">
+                <div className="flex justify-center items-center">
+                  <label className="mr-2">Don't Have Account ?</label>
+                  <NavLink
+                    to="/register"
+                    className=" text-blue-500 transition duration-500 ease-in-out  transform hover:-translate-x hover:scale-105"
+                  >
+                    Sign Up
+                  </NavLink>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,30 @@
-import React from 'react'
+import React,{ useRef, useState} from 'react'
+import { NavLink } from 'react-router-dom'
 
-function Navbar() {
+function Navbar({ name, logout }) {
+  let splitName;
+  if (name) {
+    splitName = name.split("");
+  }
+  const modalRef = useRef();
+  const blurDivRef = useRef();
+  const [ userData, setUserData ] = useState({
+    name: "",
+    email: "",
+    message: "",
+  })
+  const [ message, setMessage ] = useState("");
+  const openModal = () => {
+    modalRef.current.style.display = "block";
+    blurDivRef.current.style.display = "none"
+  };
+
+  const closeModal = ()=>{
+    modalRef.current.style.display = "none"
+    blurDivRef.current.style.display = "none"
+  };
+
+  
   return (
     <div>Navbar</div>
   )
